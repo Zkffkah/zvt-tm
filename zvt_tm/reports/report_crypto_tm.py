6b1cf85f6e7f5361471f28d26dcec5bda5005c94
lowwor <lowwor@foxmail.com>
2020-08-28 11:00:37 +0800

    feat: improve crypto report

--- zvt_tm/reports/report_crypto_tm.py
+++ zvt_tm/reports/report_crypto_tm.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
 import time
-from datetime import timedelta
+from datetime import timedelta, date
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from zvt import init_log
-from zvt.contract import IntervalLevel
-from zvt.domain import Stock
-from zvt.factors.ma.ma_factor import ImprovedMaFactor
 from zvt.factors.target_selector import TargetSelector
-from zvt.utils.pd_utils import pd_is_not_null
-from zvt.utils.time_utils import now_pd_timestamp, to_pd_timestamp
 from zvt_ccxt import Coin
 
 from zvt_tm.factors.tm_factor import TMFactor
@@ -31,20 +26,23 @@ def report_tm():
             # 抓取k线数据
             # Coin1dKdata.record_data()
 
-            target_date = now_pd_timestamp()
-
+            target_date = date.today() - timedelta(days=1)
+            start_date = target_date - timedelta(60)
             # 计算
             my_selector = TargetSelector(entity_schema=Coin, provider='ccxt',
-                                         start_timestamp='2020-01-01', end_timestamp=target_date)
+                                         start_timestamp=start_date, end_timestamp=target_date)
             # add the factors
-            tm_factor = TMFactor(entity_schema=Coin, provider='ccxt', start_timestamp='2020-01-01',
+            tm_factor = TMFactor(entity_schema=Coin, provider='ccxt',
+                                 start_timestamp=start_date,
                                  end_timestamp=target_date)
 
             my_selector.add_filter_factor(tm_factor)
 
             my_selector.run()
 
-            long_targets = my_selector.get_open_long_targets(timestamp=target_date)
+            long_targets = list(map(lambda x: x.replace('_', '.'), filter(lambda k: 'USDT' in k,
+                                                                          my_selector.get_open_long_targets(
+                                                                              timestamp=target_date))))
 
             logger.info(long_targets)
 
@@ -58,6 +56,7 @@ def report_tm():
             if error_count == 10:
                 discord_informer.send_message(f'report_tm error',
                                               'report_tm error:{}'.format(e))
+
 
 if __name__ == '__main__':
     init_log('repot_crypto_tm.log')
