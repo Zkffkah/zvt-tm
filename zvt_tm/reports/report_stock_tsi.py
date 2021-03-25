b5977bf54206dbf4980422edbf8614384db6f41e
lowwor <lowwor@foxmail.com>
2021-03-25 22:44:55 +0800

    feat: update stock tsi

--- zvt_tm/reports/report_stock_tsi.py
+++ zvt_tm/reports/report_stock_tsi.py
@@ -1,107 +1,62 @@
 # -*- coding: utf-8 -*-
 import logging
-import time
+import operator
 from datetime import timedelta
+from itertools import accumulate
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from zvt import init_log
 from zvt.contract import IntervalLevel
 from zvt.contract.api import get_entities
-from zvt.domain import Stock, StockTradeDay, Stock1dKdata, StockValuation, Block, BlockStock
-from zvt.factors.target_selector import TargetSelector
-from zvt.utils.pd_utils import pd_is_not_null
-from zvt.utils.time_utils import now_pd_timestamp, to_pd_timestamp
-from zvt_crypto import Coin
+from zvt.domain import Stock
+from zvt.utils.pd_utils import index_df
+from zvt.utils.time_utils import now_pd_timestamp
 
-from zvt_tm.factors.block_selector import BlockSelector
 from zvt_tm.factors.tsi_factor import TSIFactor
-from zvt_tm.informer.discord_informer import DiscordInformer
-from zvt_tm.informer.tonghuashun_informer import add_to_group
+from zvt_tm.informer.tradingview_informer import add_list_to_group
 
 logger = logging.getLogger(__name__)
 
 sched = BackgroundScheduler()
 
 
-@sched.scheduled_job('cron', hour=19, minute=0, day_of_week='mon-fri')
-def report_tsi():
-    while True:
-        error_count = 0
-        discord_informer = DiscordInformer()
+def to_tradingview_code(code):
+    # 上海
+    if code >= '333333':
+        return f'SSE:{code}'
+    else:
+        return f'SZSE:{code}'
 
-        try:
-            # 抓取k线数据
-            # StockTradeDay.record_data(provider='baostock', sleeping_time=2)
-            # Stock1dKdata.record_data(provider='baostock', sleeping_time=1.5)
-
-            latest_day: StockTradeDay = StockTradeDay.query_data(order=StockTradeDay.timestamp.desc(), limit=1,provider='joinquant',
-                                                                 return_type='domain')
-            if latest_day:
-                target_date = latest_day[0].timestamp
-            else:
-                target_date = now_pd_timestamp()
-
-            start_date = target_date - timedelta(360)
-
-            # 计算
-            my_selector = TargetSelector(entity_schema=Stock, provider='joinquant',
-                                         start_timestamp=start_date, end_timestamp=target_date)
-            # add the factors
-            tsi_factor = TSIFactor(entity_schema=Stock, provider='joinquant',
-                                 start_timestamp=start_date,
-                                 end_timestamp=target_date)
-
-            my_selector.add_filter_factor(tsi_factor)
-
-            my_selector.run()
-
-            long_targets = my_selector.get_open_long_targets(timestamp=target_date)
-
-            logger.info(long_targets)
-
-            msg = 'no targets'
-
-            # 过滤亏损股
-            # check StockValuation data
-            pe_date = target_date - timedelta(10)
-            if StockValuation.query_data(start_timestamp=pe_date, limit=1, return_type='domain'):
-                positive_df = StockValuation.query_data(provider='joinquant', entity_ids=long_targets,
-                                                        start_timestamp=pe_date,
-                                                        filters=[StockValuation.pe > 0],
-                                                        columns=['entity_id'])
-                bad_stocks = set(long_targets) - set(positive_df['entity_id'].tolist())
-                if bad_stocks:
-                    stocks = get_entities(provider='joinquant', entity_schema=Stock, entity_ids=bad_stocks,
-                                          return_type='domain')
-                    info = [f'{stock.name}({stock.code})' for stock in stocks]
-                    msg = '亏损股:' + ' '.join(info) + '\n'
-
-                long_stocks = set(positive_df['entity_id'].tolist())
-
-
-                if long_stocks:
-                    stocks = get_entities(provider='joinquant', entity_schema=Stock, entity_ids=long_stocks,
-                                          return_type='domain')
-                    for stock in stocks:
-                        add_to_group(stock.code, group_id='23')
-                    info = [f'{stock.name}({stock.code})' for stock in stocks]
-                    msg = msg + '盈利股:' + ' '.join(info) + '\n'
-
-            discord_informer.send_message(f'{target_date} TSI选股结果 {msg}')
-
-            break
-        except Exception as e:
-            logger.exception('report_tsi error:{}'.format(e))
-            time.sleep(60 * 3)
-            error_count = error_count + 1
-            if error_count == 10:
-                discord_informer.send_message(f'report_tsi error',
-                                              'report_tsi error:{}'.format(e))
 
 if __name__ == '__main__':
-    init_log('repot_crypto_tsi.log')
-    report_tsi()
+    print('start')
+    target_date = now_pd_timestamp() - timedelta(1)
+    start_date = target_date - timedelta(420)
+    factor = TSIFactor(entity_schema=Stock, provider='joinquant', level=IntervalLevel.LEVEL_1DAY,
+                       start_timestamp=start_date, need_persist=False)
+    df = factor.result_df
+    musts = []
+    if len(df.columns) > 1:
+        s = df.agg("and", axis="columns")
+        s.name = 'score'
+        musts.append(s.to_frame(name='score'))
+    else:
+        df.columns = ['score']
+        musts.append(df)
 
-    sched.start()
-
-    sched._thread.join()
+    filter_result = list(accumulate(musts, func=operator.__and__))[-1]
+    long_result = df[df.score == True]
+    long_result = long_result.reset_index()
+    long_result = index_df(long_result)
+    long_result = long_result.sort_values(by=['score', 'entity_id'])
+    long_result = long_result[long_result.timestamp > target_date - timedelta(8)]
+    longdf = factor.factor_df[factor.factor_df['entity_id'].isin(long_result['entity_id'].tolist())]
+    good_stocks = set(long_result['entity_id'].tolist())
+    stocks = get_entities(provider='joinquant', entity_schema=Stock, entity_ids=good_stocks,
+                          return_type='domain')
+    codeList = []
+    for stock in stocks:
+        codeList.append(to_tradingview_code(stock.code))
+    add_list_to_group(codeList, group_id=22081672)
+    info = [f'{stock.name}({stock.code})' for stock in stocks]
+    msg = '选股:' + ' '.join(info) + '\n'
+    logger.info(msg)
